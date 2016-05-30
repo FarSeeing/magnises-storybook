c03290aea4ebd83d8228f554ab71e8d3db1b96b8
Mike Sidorov <mikes.ekb@gmail.com>
2016-05-30 19:45:30 +0500

    directive!

---
+++ client/app/storybook/elframe.directive.js
@@ -0,0 +1,33 @@
+;(function(){
+  console.log('fu');
+  angular.module('storybook')
+    .directive('elframe', function(){
+      var directive = {
+        restrict: 'E',
+        controller: function() {
+        },
+        controlelrAs: 'vm',
+        template: '<iframe id="elframe"></iframe>',
+        scope: {},
+        link: function(scope, element, attrs) {
+          var iframe = element.find('iframe')[0];
+          iframe.src = element.attr('src');
+          
+         /* console.log(iframe.contentWindow.document.body);
+          iframe.onload = function(){
+            scope.$watch(
+              function(){
+              return iframe.contentWindow.document.body;//offsetHeight
+            }
+            , function(newVal){
+              console.log(newVal);
+            });
+          }*/
+          iframe.contentWindow.onWindowResize = function(h){
+            iframe.height = h;
+          }
+        }
+      }
+      return directive;
+    })
+})();
