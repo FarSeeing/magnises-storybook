00d7d2515d6b44fcc07a8aefd7277d4b6f84add0
Mike Sidorov <mikes.ekb@gmail.com>
2016-05-31 17:12:51 +0500

    margin trouble allocated

--- client/app/example/example.directive.js
+++ client/app/example/example.directive.js
@@ -13,11 +13,24 @@
           var html = document.documentElement
 
           scope.getWindowDimensions = function () {
-            return Math.max( body.scrollHeight, body.offsetHeight, 
-                             html.clientHeight, html.scrollHeight, html.offsetHeight );     
+            /*return Math.max( body.scrollHeight 
+                             , body.offsetHeight 
+                             , body.clientHeight
+                             , html.clientHeight  
+                             , html.scrollHeight 
+                             , html.offsetHeight
+                               );     */
+             //console.log(getComputedStyle(body).marginTop);
+             //here is a margin trouble
+             return body.offsetHeight + 50;
           };
-          scope.$watch(scope.getWindowDimensions, onWindowResize, true);
+          //scope.$watch(scope.getWindowDimensions, onWindowResize, true);
           var controller = $parse(elem.attr('example-wrap'))(scope);
+          
+          window.setInterval(function(){
+            onWindowResize(scope.getWindowDimensions());
+          },100);
+          //window.setInterval(function(){console.log('foo')}, 100);
 
           elem.removeAttr('example-wrap');
           elem.attr('ng-controller', controller);
@@ -28,7 +41,6 @@
 
   function onWindowResize(height) {
     var frame = window.frameElement;
-    console.log('lol');
     if (frame) {
       frame.style.height = height + 'px';
     }
