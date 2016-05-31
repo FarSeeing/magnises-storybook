8de76f9d167c39eed05a5b62f9ce78970bd8fcd1
Mike Sidorov <mikes.ekb@gmail.com>
2016-05-31 17:21:33 +0500

    documentation structure first example

--- client/app/components/action-button/actionButton.doc.js
+++ client/app/components/action-button/actionButton.doc.js
@@ -7,16 +7,16 @@
       .Story('action button')
       .add({
         name: 'action button states',
-        templateUrl: 'app/components/action-button/actionButton.state.doc.html',
+        templateUrl: 'app/components/action-button/examples/actionButton.state.doc.html',
         controller: 'ActionButtonDocController',
         controllerAs: 'vm',
-        documentationUrl: 'app/components/action-button/actionButton.state.doc.annotation.md'
+        documentationUrl: 'app/components/action-button/doc/actionButton.state.doc.annotation.md'
       })
       .add({
         name: 'action button animation',
-        templateUrl: 'app/components/action-button/actionButton.animation.doc.html',
+        templateUrl: 'app/components/action-button/examples/actionButton.animation.doc.html',
         controller: 'ActionButtonDocController',
-        documentationUrl: 'app/components/action-button/actionButton.animation.doc.annotation.md'
+        documentationUrl: 'app/components/action-button/doc/actionButton.animation.doc.annotation.md'
       }).publish();
   })
 })();
